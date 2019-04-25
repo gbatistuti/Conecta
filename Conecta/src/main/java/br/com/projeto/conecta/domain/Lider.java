7fff939920aa0f7f36b7058662744bb8108bb66f
Jacqueline Lima Prates <jacqueline.prates@totvs.com.br>
2019-04-25 15:20:22 -0300

    adiciona classes de dominio e cria repository

--- Conecta/src/main/java/br/com/projeto/conecta/domain/Lider.java
+++ Conecta/src/main/java/br/com/projeto/conecta/domain/Lider.java
@@ -1,71 +1,17 @@
 package br.com.projeto.conecta.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "lideres")
-public class Lider {
+public class Lider extends Usuarios{
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	private Integer codLider;
-	private String nome;
-	private String email;
-	private String senha;
 	private String unidade;
-	
-	public Lider(Integer id, Integer codLider, String nome, String email, String senha, String unidade) {
-		this.id = id;
-		this.codLider = codLider;
-		this.nome = nome;
-		this.email = email;
-		this.senha = senha;
+
+	public Lider(String email, String senha, Integer id, Integer codigo, String nome, String unidade) {
+		super(email, senha, id, codigo, nome);
 		this.unidade = unidade;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Integer getCodLider() {
-		return codLider;
-	}
-
-	public void setCodLider(Integer codLider) {
-		this.codLider = codLider;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
 	}
 
 	public String getUnidade() {
@@ -75,6 +21,5 @@ public class Lider {
 	public void setUnidade(String unidade) {
 		this.unidade = unidade;
 	}
-
 	
 }

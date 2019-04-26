c00b923edf2a245d7b888df72de4cf7e0ea7d426
Jacqueline Lima Prates <jacqueline.prates@totvs.com.br>
2019-04-26 20:39:42 -0300

    implementa hibernate nas classes de dominio

--- Conecta/src/main/java/br/com/projeto/conecta/domain/Consultor.java
+++ Conecta/src/main/java/br/com/projeto/conecta/domain/Consultor.java
@@ -4,27 +4,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="recursos")
+@PrimaryKeyJoinColumn(name="idUsuario")
 public class Consultor extends Usuarios{
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Integer id;
 	
 	private String unidade;
 	private String cargo;
 	private Float preco;
-	
-	public Consultor(String email, String senha, Integer codigo, String nome, Integer id, String unidade, String cargo,
-			Float preco) {
+
+	public Consultor(String email, String senha, Integer codigo, String nome, String unidade, String cargo, Float preco) {
 		super(email, senha, codigo, nome);
-		this.id = id;
 		this.unidade = unidade;
 		this.cargo = cargo;
 		this.preco = preco;
 	}
+	
+
+	
+//	   @OneToOne
+//	   @JoinColumn(name = "FK_USUARIO")
+//	   private Usuarios usuarios;
 	
 }

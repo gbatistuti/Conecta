6a57452c2823049ea2009041eeb3136b7f2b90db
gbatistuti <gustavo.batistuti1@gmail.com>
2019-05-07 11:37:34 -0300

    Implementação lista de projetos

--- Conecta/src/main/java/br/com/projeto/conecta/repository/ProjetoRepository.java
+++ Conecta/src/main/java/br/com/projeto/conecta/repository/ProjetoRepository.java
@@ -1,11 +1,17 @@
 package br.com.projeto.conecta.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.projeto.conecta.domain.Projeto;
 
 @Repository
 public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {
+
+//	@Query(value = "from projetos where id_usuario = ?1" )
+//	List<Projeto> buscarByIdLider();
 
 }
--- Conecta/src/main/java/br/com/projeto/conecta/service/ProjetoService.java
+++ Conecta/src/main/java/br/com/projeto/conecta/service/ProjetoService.java
@@ -1,14 +1,8 @@
 package br.com.projeto.conecta.service;
 
-import java.util.List;
-
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.projeto.conecta.domain.Demanda;
-import br.com.projeto.conecta.domain.Projeto;
 import br.com.projeto.conecta.repository.ProjetoRepository;
 
 @Service
@@ -17,17 +11,10 @@ public class ProjetoService {
 	@Autowired
 	private ProjetoRepository projetoRepository;
 	
-	@Transactional
-	public List<Projeto> buscarTodos(){
+	public Object buscarTodos() {
 		return projetoRepository.findAll();
 	}
 	
-	public void excluir(Integer id) {
-		projetoRepository.deleteById(id);
-	}
-	
-//	public void editar(Integer id) {
-//		projetoRepository.updateById(id);
-//	}
+
 
 }

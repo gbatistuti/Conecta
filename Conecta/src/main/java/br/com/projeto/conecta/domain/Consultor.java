72e2742fc89e6508ba265a055fc6958bf27a23ee
gbatistuti <gustavo.batistuti1@gmail.com>
2019-05-07 14:41:45 -0300

    Correções ortográficas

--- Conecta/src/main/java/br/com/projeto/conecta/domain/Consultor.java
+++ Conecta/src/main/java/br/com/projeto/conecta/domain/Consultor.java
@@ -1,9 +1,6 @@
 package br.com.projeto.conecta.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 

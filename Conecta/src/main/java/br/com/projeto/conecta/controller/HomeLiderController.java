68dc7ec095c4625a6ad66bd4a7bab9558a1fc18c
Elias Angelo De Oliveira <oliveira.elias@totvs.com.br>
2019-05-13 13:17:20 -0300

    implementa lista de agendamento

--- Conecta/src/main/java/br/com/projeto/conecta/controller/HomeLiderController.java
+++ Conecta/src/main/java/br/com/projeto/conecta/controller/HomeLiderController.java
@@ -5,16 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.projeto.conecta.service.AgendamentoService;
+
 @Controller
 public class HomeLiderController {
 
-//	@Autowired
-//	private DemandaService demandaService;
-//	
-//	@GetMapping("/homeLider")
-//	public String listar(ModelMap model) {
-//		model.addAttribute("demandas", demandaService.buscarTodos());
-//		return "homeLider";
-//	}
-	
+	@Autowired
+	private AgendamentoService agendamentoService;
+
+	@GetMapping("/homeLider")
+	public String ListarAgendamentos(ModelMap model) {
+		model.addAttribute("agendamento", agendamentoService.BuscarTodos());
+		return "homeLider";
+	}
 }

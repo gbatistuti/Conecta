40494f82a622944a4cb835f06f71962912de3c08
Lucas Antunes Campos <campos.lucas@totvs.com.br>
2019-06-07 16:00:50 -0300

    Função do botão de abilidades funcionando com pop-up

---
+++ Conecta/src/main/java/br/com/projeto/conecta/config/CacheConfig.java
@@ -0,0 +1,29 @@
+package br.com.projeto.conecta.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+	
+	@Bean
+	public CacheManager cacheManager() {
+		SimpleCacheManager cacheManager = new SimpleCacheManager();
+		List<Cache> cacheList = new ArrayList<Cache>();
+		cacheList.add(new ConcurrentMapCache("userCache"));
+		cacheList.add(new ConcurrentMapCache("consultorCache"));
+		cacheList.add(new ConcurrentMapCache("liderCache"));
+		cacheManager.setCaches(cacheList);
+		return cacheManager;
+	}
+
+}

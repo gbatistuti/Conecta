524554bf0a6a5ce570ab636e0c37f9b74d6f9b76
gbatistuti <gustavo.batistuti1@gmail.com>
2019-06-24 09:51:26 -0300

    Trocando o sessionStorage por localStorage

--- Conecta/src/main/resources/static/js/menu.js
+++ Conecta/src/main/resources/static/js/menu.js
@@ -1,28 +1,28 @@
 function imagem1(){
 	var imagem = 'url("../img/menu/fundo1.png")'
-	window.sessionStorage.clear();
-	window.sessionStorage.setItem('fundo1', imagem)
+	window.localStorage.clear();
+	window.localStorage.setItem('fundo1', imagem)
     document.body.style.backgroundImage = imagem
 }
 
 function imagem2(){
 	var imagem = 'url("../img/menu/fundo2.png")'
-	window.sessionStorage.clear();
-	window.sessionStorage.setItem('fundo2', imagem)
+	window.localStorage.clear();
+	window.localStorage.setItem('fundo2', imagem)
 	document.body.style.backgroundImage = imagem
 }
 
 function imagem3(){
 	var imagem = 'url("../img/menu/fundo3.png")'
-	window.sessionStorage.clear();
-	window.sessionStorage.setItem('fundo3', imagem)
+	window.localStorage.clear();
+	window.localStorage.setItem('fundo3', imagem)
 	document.body.style.backgroundImage = imagem
 }
 
 function imagem4(){
 	var imagem = 'url("../img/menu/fundo4.png")'
-	window.sessionStorage.clear();
-	window.sessionStorage.setItem('fundo4', imagem)
+	window.localStorage.clear();
+	window.localStorage.setItem('fundo4', imagem)
 	document.body.style.backgroundImage = imagem
 }
 

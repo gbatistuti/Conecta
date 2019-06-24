6e7d4dd79739936e2ef370f7c990abd446db54cf
gbatistuti <gustavo.batistuti1@gmail.com>
2019-06-24 09:21:30 -0300

    Menu parametrizável funcionando - com checked

--- Conecta/src/main/resources/static/js/menu.js
+++ Conecta/src/main/resources/static/js/menu.js
@@ -1,28 +1,28 @@
 function imagem1(){
 	var imagem = 'url("../img/menu/fundo1.png")'
-	window.sessionStorage.removeItem('fundo')
-	window.sessionStorage.setItem('fundo', imagem)
+	window.sessionStorage.clear();
+	window.sessionStorage.setItem('fundo1', imagem)
     document.body.style.backgroundImage = imagem
 }
 
 function imagem2(){
 	var imagem = 'url("../img/menu/fundo2.png")'
-	window.sessionStorage.removeItem('fundo')
-	window.sessionStorage.setItem('fundo', imagem)
+	window.sessionStorage.clear();
+	window.sessionStorage.setItem('fundo2', imagem)
 	document.body.style.backgroundImage = imagem
 }
 
 function imagem3(){
 	var imagem = 'url("../img/menu/fundo3.png")'
-	window.sessionStorage.removeItem('fundo')
-	window.sessionStorage.setItem('fundo', imagem)
+	window.sessionStorage.clear();
+	window.sessionStorage.setItem('fundo3', imagem)
 	document.body.style.backgroundImage = imagem
 }
 
 function imagem4(){
 	var imagem = 'url("../img/menu/fundo4.png")'
-	window.sessionStorage.removeItem('fundo')
-	window.sessionStorage.setItem('fundo', imagem)
+	window.sessionStorage.clear();
+	window.sessionStorage.setItem('fundo4', imagem)
 	document.body.style.backgroundImage = imagem
 }
 
@@ -35,4 +35,6 @@ function VisibilidadePainelUser(){
     }
     
 }
+
+
 
